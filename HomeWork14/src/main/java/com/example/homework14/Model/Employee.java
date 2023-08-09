c93aabab7d8da34f712018772e78d42b05d10c07
ayob9956 <139464497+ayob9956@users.noreply.github.com>
2023-08-09 23:56:38 +0300

    HomeWork14 after edit!

--- HomeWork14/src/main/java/com/example/homework14/Model/Employee.java
+++ HomeWork14/src/main/java/com/example/homework14/Model/Employee.java
@@ -10,11 +10,11 @@ import org.springframework.format.annotation.NumberFormat;
 public class Employee {
     @NotEmpty(message = "id sholed not emptey")
 
-    @Min(value = 3,message = "id sholed gerthers than 3!!!!")
+    @Size(min = 3,max = 10,message = "id sholed gerthers than 3!!!!")
     private String ID;
     @NotEmpty(message = "name sholed not emptey")
 //    @Min(value = 4,message = "should name gerther than 4")
-    @Size(min = 4,message = "should name gerther than 4")
+    @Size(min = 4,max = 15,message = "should name gerther than 4")
     private String name;
     @NotEmpty(message = "postion shold not emptey!")
     @Pattern(regexp = "(supervisor|coordinator)", message = "user role not valid!")
@@ -23,9 +23,10 @@ public class Employee {
     private boolean onLeave = false;
     @NotNull(message = " emp years shold be not null")
     @Max(value = 2023,message = "emp years sholed be lees than 2023")
+    @Min(value = 1950,message = "emp years sholed be grether than 1950" )
     private int employmentYear;
     @NotNull(message = "sholed age not null")
-    @Min(value = 25,message = "age sholed greter than 25")
+    @Min(value = 26,message = "age sholed greter than 25")
     private int age;
     @NotNull(message = "annul sholed not null")
     private int annualLeave;
